64447fbbeb4fe6eaa8bd7ee60f331283dd5f62cb
lauricioweb <lauriciomalek@gmail.com>
2025-02-16 17:56:19 -0300

    replace

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,8 +1,14 @@
+require("dotenv").config(); // Carregar variáveis de ambiente
 const { Sequelize } = require("sequelize");
 
-const connection = new Sequelize("db_list_task", "root", "Zxc@123zxc123", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const connection = new Sequelize(
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASS, 
+  {
+    host: process.env.DB_HOST, 
+    dialect: process.env.DB_DIALECT, 
+  }
+);
 
 module.exports = connection;
